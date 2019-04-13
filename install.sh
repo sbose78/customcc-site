ddb9133e0141f4e233af123101a38da0baedb5f4
Shoubhik Bose <shbose@redhat.com>
2019-04-12 22:30:05 -0400

    install yarn

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 git clone https://github.com/codeforboston/communityconnect
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources
+
+sudo apt-get update && sudo apt-get install yarn
+
 cd communityconnect
 yarn build
